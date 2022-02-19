fe47cbb291b36592b1242c9933f252bb89a7ab28
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-02-19 12:21:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const theme = window.localStorage.getItem("theme");
 se sim aplica o tema escuro ao body */
 if (theme === "dark") document.body.classList.add("dark");
 
-// event listener para quando o botão de alterar o tema for clicado
+// event listener para quando o botão de alterar o tema for clicado.
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   if (theme === "dark") {
